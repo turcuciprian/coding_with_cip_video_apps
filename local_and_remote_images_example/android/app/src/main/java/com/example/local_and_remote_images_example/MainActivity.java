6daa51070e7748b2b79a12e479fb08bab7a0d1cc
Ciprian Turcu <doxtitech@gmail.com>
2019-08-17 23:01:13 +0300

    load local and remote images to flutter

---
+++ local_and_remote_images_example/android/app/src/main/java/com/example/local_and_remote_images_example/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.local_and_remote_images_example;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
