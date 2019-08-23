cf1579697ca02acfe3ee633e81342afc1d846519
Ciprian Turcu <doxtitech@gmail.com>
2019-08-24 00:30:18 +0300

    final modifications for provider state management project example

--- state_management_with_provider_example/lib/countData.dart
+++ state_management_with_provider_example/lib/countData.dart
@@ -4,16 +4,13 @@ import 'package:provider/provider.dart';
 class CountData with ChangeNotifier {
   int currentCounter = 0;
   List<int> listOfCounters = new List<int>();
+
   void addCounter() {
-    if (listOfCounters.length == 0 ||
-        currentCounter != listOfCounters[listOfCounters.length - 1]) {
-      listOfCounters.add(this.currentCounter);
-      notifyListeners();
-    }
+    listOfCounters.add(currentCounter);
+    notifyListeners();
   }
 
-  void setCurrentCounter(value) {
-    currentCounter = value;
-    notifyListeners();
+  void setLatestValue(newValue) {
+    currentCounter = newValue;
   }
 }
--- state_management_with_provider_example/lib/main.dart
+++ state_management_with_provider_example/lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:state_management_with_provider_example/countData.dart';
+import 'countData.dart';
 
-void main() => runApp(ChangeNotifierProvider(
-      builder: (context) => CountData(),
-      child: MyApp(),
-    ));
+void main() => runApp(
+    ChangeNotifierProvider(builder: (context) => CountData(), child: MyApp()));
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -14,15 +12,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
@@ -32,15 +21,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -54,56 +34,29 @@ class _MyHomePageState extends State<MyHomePage> {
   void _incrementCounter() {
     setState(() {
       _counter++;
-      Provider.of<CountData>(context, listen: false)
-          .setCurrentCounter(_counter);
+      Provider.of<CountData>(context, listen: false).setLatestValue(_counter);
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Consumer<CountData>(builder: (context, countDataLink, _) {
       return Scaffold(
         appBar: AppBar(
-          // Here we take the value from the MyHomePage object that was created by
-          // the App.build method, and use it to set our appbar title.
           title: Text(widget.title),
         ),
         body: Center(
-          // Center is a layout widget. It takes a single child and positions it
-          // in the middle of the parent.
           child: Column(
-            // Column is also layout widget. It takes a list of children and
-            // arranges them vertically. By default, it sizes itself to fit its
-            // children horizontally, and tries to be as tall as its parent.
-            //
-            // Invoke "debug painting" (press "p" in the console, choose the
-            // "Toggle Debug Paint" action from the Flutter Inspector in Android
-            // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-            // to see the wireframe for each widget.
-            //
-            // Column has various properties to control how it sizes itself and
-            // how it positions its children. Here we use mainAxisAlignment to
-            // center the children vertically; the main axis here is the vertical
-            // axis because Columns are vertical (the cross axis would be
-            // horizontal).
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               RaisedButton(
-                onPressed: () {
-                  setState(() {
+                  onPressed: () {
                     countDataLink.addCounter();
-                    _counter = 0;
-
-                  });
-                },
-                child: Text('Save and reset'),
-              ),
+                    setState(() {
+                      _counter = 0;
+                    });
+                  },
+                  child: Text('Save and Reset')),
               Text(
                 'You have pushed the button this many times:',
               ),
@@ -117,26 +70,29 @@ class _MyHomePageState extends State<MyHomePage> {
                       context,
                       MaterialPageRoute(
                           builder: (context) => Scaffold(
-                                appBar: AppBar(title: Text('Second page')),
+                                appBar: AppBar(
+                                  title: Text('Second Page'),
+                                ),
                                 body: Column(
                                   children: <Widget>[
                                     Text(
-                                        'Latest Value: ${countDataLink.currentCounter}'),
+                                        'Last Counter Set: ${countDataLink.currentCounter.toString()}'),
                                     new Expanded(
-                                      child: ListView.builder(
-                                        itemCount:
-                                            countDataLink.listOfCounters.length,
-                                            itemBuilder: (BuildContext ctxt, int index){
-                                              return Text('Set: ${countDataLink.listOfCounters[index].toString()}');
-                                            },
-                                      ),
-                                    )
+                                        child: ListView.builder(
+                                      itemCount:
+                                          countDataLink.listOfCounters.length,
+                                      itemBuilder:
+                                          (BuildContext ctxt, int index) {
+                                        return Text(
+                                            ' Set $index: ${countDataLink.listOfCounters[index].toString()}');
+                                      },
+                                    ))
                                   ],
                                 ),
                               )));
                 },
-                child: Text('View all sets'),
-              )
+                child: Text('View All Counted Sets'),
+              ),
             ],
           ),
         ),
