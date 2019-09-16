90c4229331372f5b1702b47063333a000cf58dba
Ciprian Turcu <doxtitech@gmail.com>
2019-09-16 20:07:24 +0300

    rasp pi 4 stream to flutter

---
+++ raspberry_pi_stream_camera_to_flutter_live_example/android/app/src/main/kotlin/com/example/raspberry_pi_stream_camera_to_flutter_live_example/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.raspberry_pi_stream_camera_to_flutter_live_example
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
