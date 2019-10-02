61d08f89e30cc75ef28affd77530f146105c4a79
Ciprian Turcu <doxtitech@gmail.com>
2019-10-02 13:01:18 +0300

    searching a list of objects in flutter

---
+++ searching_a_274k_list_of_objects/android/app/src/main/kotlin/com/example/searching_a_274k_list_of_objects/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.searching_a_274k_list_of_objects
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
